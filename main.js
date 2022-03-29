27a0cb192f9a2bab12d6fecb89053a5806d4cecf
hanahhhhhhhh <hanguyen18052004@gmail.com>
2022-03-29 07:01:23 +0700

    messege

---
+++ main.js
@@ -0,0 +1,8 @@
+let r,s,p;
+r = prompt('banh kinh hinh tron la: ');
+s = 3.14 * r * r;
+p = 2 *3.14 *r;
+document.write('S circle: '+s.toFixed(2));
+document.write('</br>');
+document.write('Chu vi hinh tron: '+p.toFixed(2));
+
